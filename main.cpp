ecf780364e0ade4aa989ba2d211409cd287e4b9b
Yoyo Yuan <yoyoyuan1729@gmail.com>
2025-07-04 18:27:53 -0700

    added comments

--- main.cpp
+++ main.cpp
@@ -3,15 +3,25 @@
 class BaseComponent; // declared before Controller.
 
 class IController{
-    // sender, and event.
+    // takes a pointer to the base component, and event.
     public: 
-    virtual void Notify(BaseComponent*sender, std::string event) const = 0;// event is a string. it is an interface
-}
+    virtual void Notify(BaseComponent* sender, std::string event) const = 0;// event is a string. it is an interface
+    // virtual: to be overriden by subclasses. a virutal fn can't be instantiated.
+    // void: no return value
+    // = 0: this function must be overriden.
+    // event is a string. std is the namespace (like a Python module), and :: is the scope resolution similar to . in Python.
+};
 
 class BaseComponent{ 
     // references the controller.
-    protected;
-    IController* controller;
+    protected: // protected: can be accessed by itself and children.
+    IController* _controller;
+    
     public:
-    BaseComponent(IController* controller = nullptr): _controller(controller){}
+    BaseComponent(IController* controller = nullptr) // name equals the classname. this is the init method.
+        : _controller(controller){}
+    
+    void set_controller(IController* controller){
+        _controller = controller;
+    }
 }
