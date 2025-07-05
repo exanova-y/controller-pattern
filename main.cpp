339e620cbc7aad6f8f180e12929f456a4d70ddd2
Yoyo Yuan <yoyoyuan1729@gmail.com>
2025-07-04 18:56:31 -0700

    controller interaction with mediators

--- main.cpp
+++ main.cpp
@@ -28,15 +28,58 @@ class BaseComponent{
 };
 
 
-class ArmenianComponent: public BaseComponent{
+class Armenians: public BaseComponent{
     public:
-    void RecruitArmenians(){ // погнали!! Let's go!
-        std::cout << "Armenian is recruiting Armenians successfully!";
-        _controller->Notify(this, "recruiting Armenians");
+    void HostSlavicReunion(){ // погнали!! Let's go!
+        std::cout << "Armenians are hosting a slavic reunion!"; // this is just print. std namespace (like a module 'math'). <<, the insertion operator sends text to it.
+        _controller->Notify(this, "hosting reunion");
     }
 
-    void HostReunion(){
-        std::cout << "Armenian is hosting a slavic reunion successfully!";
-        _controller->Notify(this, "hosting a slavic reunion");
+    void RecruitingArmenianCultists(){
+        std::cout << "Armenia is recruiting Armenian cultists.";
+        _controller->Notify(this, "recruit cultists");
+    }
+
+};
+
+class Kazakhs: public BaseComponent{
+    public:
+    void AttendSlavicReunion(){
+        std::cout << "Kazakhs are attending a Slavic reunion!";
+        _controller->Notify(this, "attend reunion");
+    }
+
+    void SharePostOnLinkedIn(){
+        std::cout << "Kazakhs are sharing this post on LinkedIn!";
+        _controller->Notify(this, "linkedin resharing");
+    }
+
+    void HostCentralAsianLadiesInitiative(){
+        std::cout << "Kazakhs are hosting a Central Asian Ladies Initiative!";
+        _controller->Notify(this, "hosting CALI");
     }
 };
+
+class TigranTheThird: public IController{
+    // a concrete controller.
+    private: 
+    Armenians* armenians;
+    Kazakhs* kazakhs;
+
+    public:
+    TigranTheThird(Armenians* am, Kazakhs* kz)
+    : armenians(am), kazakhs(kz){
+        armenians->set_controller(this);
+        kazakhs->set_controller(this); // the components allow Tigran to control them.
+    } // instantiate army
+
+    void Notify(BaseComponent* sender, std::string event) const override{
+        std::cout << "event received:" << event << std::endl;
+        if(event == "hosting reunion"){
+            kazakhs->AttendSlavicReunion();
+        }else if(event == "recruit cultists"){
+            kazakhs->SharePostOnLinkedIn();
+        }
+    }
+};
+
